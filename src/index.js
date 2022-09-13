2bafdf2859ad690c6ab0e59403e91315f21dee37
Scout Cox <coxscout@gmail.com>
2022-09-13 12:05:39 -0400

    Switching branches

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 const xcolors = "https://x-colors.herokuapp.com/api/random";
 
-function randomColor() {
-  return fetch(xcolors).then((res) => res.json());
+// fetch random color
+function getColor(number) {
+  return fetch(xcolors + `/${number}`).then((res) => res.json());
 }
 
-function randomHue(hue) {
+// fetch random colors by hue
+function getHue(hue) {
   return fetch(xcolors + `/${hue}`).then((res) => res.json());
 }
 
-// randomColor().then((json) => console.log(json));
-// randomHue("blue").then((json) => console.log(json));
+getColor().then((json) => console.log(json));
+getHue("blue").then((json) => console.log(json));
 
 // populates color cards on launch
 function onLaunch() {
-  randomColor().then(launchColor).then(exploreColors);
+  getColor().then(launchColor).then(exploreColors);
 }
 
 // displays launch color card
-function launchColor() {}
+function launchColor() {
+  getColor();
+}
 
 // displays explore color cards
-function exploreColors() {}
+function exploreColors(color) {
+  let hex = document.getElementById("hex");
+  hex.innerText = color.hex;
+
+  let rgb = document.getElementById("rgb");
+  rgb.innerText = color.hsl;
+
+  let hsl = document.getElementById("hsl");
+  hsl.innerText = color.hsl;
+}
 
 // displays launch color card filtered by hue
 function launchHue() {}
@@ -31,20 +44,26 @@ function exploreHue() {}
 // toggle between light & dark mode
 // needs .dark-mode in CSS to work
 function lightDarkMode() {
-  const bodyMode = document.body;
-  bodyMode.classList.toggle("dark-mode");
+  const mode = document.body;
+  mode.classList.toggle("dark-mode");
 }
 
 // explore card hover event
-const exploreCards = document.getElementById("explore-cards")
-exploreCards.addEventListener("mouseover", function () {
-  // zoom function here - CSS?
-});
+// const exploreCards = document.getElementById("explore-cards");
+// exploreCards.addEventListener("mouseover", function () {
+//   // zoom function here - CSS?
+// });
 
+// getting data back from the promise and looping through it to render explore cards
+// getColor().then((random) => {
+//     random.forEach((color) => {
+//       exploreColors(color);
+//     });
+//   });
 
-randomColor().then((colors) => {
-  colors.forEach((card) => {
-    launchColor(card);
-    exploreColors(card);
-  });
-})
+// etc.
+
+/*
+
+  
+*/
