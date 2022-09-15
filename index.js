93134594c9a2772748c39c9521b1cf689551b01f
Scout Cox <coxscout@gmail.com>
2022-09-15 15:46:08 -0400

    Wherein Scout discovers CSS variables and data-* attributes after a whole day of doing it the hard way

--- index.js
+++ index.js
@@ -10,80 +10,15 @@ function getHue(hue) {
   return fetch(xcolors + `/${hue}`).then((res) => res.json());
 }
 
-// getColor().then((json) => console.log(json));
-// getHue("blue").then((json) => console.log(json));
+const htmlTag = document.firstElementChild;
 
-// populates color cards on launch
-// function onLaunch() {
-//   getColor().then(launchColor).then(exploreColors);
-// }
-
-// // displays launch color card
-// function launchColor() {
-//   getColor();
-// }
-
-// displays explore color cards (WIP)
-// function exploreColors(color) {
-//   let hex = document.getElementById("hex");
-//   hex.innerHTML = `HEX: ${color.hex}`;
-
-//   let rgb = document.getElementById("rgb");
-//   rgb.innerHTML = `RGB: ${color.hsl}`;
-
-//   let hsl = document.getElementById("hsl");
-//   hsl.innerHTML = `HSL: ${color.hsl}`;
-// }
-
-// displays launch color card filtered by hue
-// function launchHue() {}
-
-// displays explore color cards filtered by hue
-// function exploreHue() {}
-
-// toggle between light & dark mode
 function toggleAppearance() {
-  const body = document.body;
-  const header = document.querySelector("header");
-  const nav = document.querySelector("nav");
-  const toggleBtn = document.querySelector("div.toggleAppearance");
-  const logo = document.querySelector("a.logo");
-  const exploreCont = document.querySelector("div.exploreContainer");
-  const footer = document.querySelector("footer");
-  //   const refresh = document.querySelector("button.refreshBtn");
-  //   const cards = document.getElementsByClassName("cards");
-  //   const homeColor = document.querySelector("");
-  //   const exploreBtn = document.querySelector(".exploreBtn");
-  //   const hueFilter = document.querySelector("div.filter.toggle");
-  //   const hueList = document.querySelector("a.link");
-
-  header.classList.toggle("dark-mode");
-  body.classList.toggle("dark-mode");
-  nav.classList.toggle("dark-mode");
-  toggleBtn.classList.toggle("dark-mode");
-  logo.classList.toggle("dark-mode");
-  exploreCont.classList.toggle("dark-mode");
-  footer.classList.toggle("dark-mode");
-
-  //   refresh.classList.toggle("dark-mode");
-  //   cards
-  //   homeColor
-  //   exploreBtn.classList.toggle("dark-mode");
-  //   hueFilter.classList.toggle("dark-mode");
-  //   hueList.classList.toggle("dark-mode");
-
-  console.log("test");
+  if (htmlTag.dataset.theme == "light") {
+    htmlTag.dataset.theme = "dark";
+  } else {
+    htmlTag.dataset.theme = "light";
+  }
 }
-
-//   homeColor.classList.toggle("dark-mode");
-//   explore.classList.toggle("dark-mode");
-
-//   if (toggleBtn.dark - mode === true) {
-//     toggleBtn.innerText = "☾";
-//   } else {
-//     toggleBtn.innerText = "☀";
-//   }
-
 
 toggleFilter();
 scrollHeader();
